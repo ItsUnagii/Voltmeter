f708d9fd2de6e69f60472cdf2aef4e32a2a3088e
Aidan Lee <36676470+ItsUnagii@users.noreply.github.com>
2025-03-10 21:50:24 -0700

    Update Voltmeter.ino

--- Voltmeter.ino
+++ Voltmeter.ino
@@ -1,13 +1,13 @@
 // Define pins
 #define POS_PIN A0   // Voltage divider output
 #define NEG_PIN A1
-#define MOSFET_PIN 3   // MOSFET gate control pin
+#define MOSFET_PIN 7   // MOSFET gate control pin
 
 // Resistor values
-const float R1 = 9930;   // 10kΩ
-const float R2 = 46590;   // 47kΩ
+const float R1 = 9930; //10000;    // 10kΩ
+const float R2 = 46590; // 47000;    // 47kΩ
 const float MOSFET = 2.20;
-const float voltageDividerRatio = 1.0169823232323232323232323232323; // (R1 + (R2 + MOSFET)) / (R2 + MOSFET);
+const float voltageDividerRatio = (R1 + (R2 + MOSFET)) / (R2 + MOSFET) * (3.645 / 3.569); //1.0169823232323232323232323232323; 
 
 void setup() {
     Serial.begin(9600);
@@ -19,21 +19,37 @@ void loop() {
     digitalWrite(MOSFET_PIN, HIGH); // Turn MOSFET ON
     delay(750); // Stabilization time
 
-    pinMode(POS_PIN, OUTPUT);
-    digitalWrite(POS_PIN, LOW); // Discharge floating state
-    delay(1);
-    pinMode(POS_PIN, INPUT);
+    
 
-    analogRead(POS_PIN);  // Dummy read
-    int posValue = analogRead(POS_PIN);  // Actual read
-    float posVoltage = (posValue * 5.0) / 1023.0;
-    float batteryVoltage = posVoltage * voltageDividerRatio;
+    float sum = 0;
+    for (int i = 0; i < 20; i++) {
+      pinMode(POS_PIN, OUTPUT);
+      digitalWrite(POS_PIN, LOW); // Discharge floating state
+      delay(1);
+      pinMode(POS_PIN, INPUT);
+
+      analogRead(POS_PIN);  // Dummy read
+      int posValue = analogRead(POS_PIN);  // Actual read
+      float posVoltage = (posValue * 5.0) / 1023.0;
+      float batteryVoltage = posVoltage * voltageDividerRatio;
+      // Serial.print("Battery Voltage: ");
+      // Serial.print(batteryVoltage, 3);
+      // Serial.println(" V");
+      
+      sum += batteryVoltage;
+    }
+
+    float time = millis(); // KEEP THIS ON A FLOAT YOU DINGUS
+    // Serial.println(time);
+    float hours = time / 3600000;
+
+    Serial.print(hours, 3);
+    Serial.print(", ");
+    Serial.println(sum / 20, 3);
+    
 
     digitalWrite(MOSFET_PIN, LOW); // Turn MOSFET OFF
 
-    Serial.print("Battery Voltage: ");
-    Serial.print(batteryVoltage, 3);
-    Serial.println(" V");
-
-    delay(1000);
+    //delay(2000);
+    delay(3600000 * 2);
 }
